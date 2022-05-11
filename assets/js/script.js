4f5b767c33ceab0dc5612eecbe91fceb25e4ba20
Angela Gola <angelagola.ko@gmail.com>
2022-05-11 19:50:27 -0400

    Adding apis

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// var query='hello';
+
+// async function getTranslation(query){
+    
+//     const res = await fetch("https://libretranslate.com/translate", {
+//         method: "POST",
+//         body: JSON.stringify({
+//             q: query,
+//             source: "en",
+//             target: "es",
+//             format: "text"
+//         }),
+//         headers: { "Content-Type": "application/json" }
+//     });
+//     console.log(await res.json());
+// }
+
+// getTranslation(query);
