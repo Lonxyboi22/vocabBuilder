75cd16fc1bbc48481a35b651bc7857d220370ab9
Lonxyboi <drewthemachine22@gmail.com>
2022-05-11 20:19:43 -0400

    add dictionary api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-// var query='hello';
-
+const query = "lame";
 // async function getTranslation(query){
-    
-//     const res = await fetch("https://libretranslate.com/translate", {
-//         method: "POST",
-//         body: JSON.stringify({
-//             q: query,
-//             source: "en",
-//             target: "es",
-//             format: "text"
-//         }),
-//         headers: { "Content-Type": "application/json" }
-//     });
-//     console.log(await res.json());
-// }
-
+//     function getTranslation(query){
+//         const res = await fetch("https://libretranslate.com/translate", {
+//             method: "POST",
+//             body: JSON.stringify({
+//                 q: query,
+//                 source: "en",
+//                 target: "es",
+//                 format: "text"
+//             }),
+//             headers: { "Content-Type": "application/json" }
+//         });
+//         console.log(await res.json());
+//     }
+   
+// // }
 // getTranslation(query);
+
+// https://api.dictionaryapi.dev/api/v2/entries/en/
+
+
+function definition(){
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+ query)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
