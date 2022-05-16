73e11471bd252e174981beae9888d65c65b76ed0
rstanbaugh <rich_bootcamp@icloud.com>
2022-05-16 13:17:49 -0400

    generate random words from suggested words list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
-// var query='hello';
+const query = "lame";
 
-// async function getTranslation(query){
-    
-//     const res = await fetch("https://libretranslate.com/translate", {
-//         method: "POST",
-//         body: JSON.stringify({
-//             q: query,
-//             source: "en",
-//             target: "es",
-//             format: "text"
-//         }),
-//         headers: { "Content-Type": "application/json" }
-//     });
-//     console.log(await res.json());
-// }
 
-// getTranslation(query);
+
+
+function definition(){
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+ query)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+
+var randomWord = function(){
+    return word = suggestedWords[Math.round(Math.random()*suggestedWords.length)];
+};
